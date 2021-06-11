e1601eb1932dd99e49a4e28b0005be41337a274c
heiheihei <980987346@qq.com>
2021-06-11 21:41:54 +0800

    update 0.2 绘画

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,7 @@ import java.awt.event.WindowEvent;
 
 public class Main {
     public static void main(String[] args) {
-        Frame frame = new Frame("Tank War");
-        frame.setSize(800,600);
-        frame.setResizable(false);
-        frame.setVisible(true);
+        TankFrame tankFrame = new  TankFrame();
 
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
     }
 }
