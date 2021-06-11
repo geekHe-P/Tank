6379a2072b62e225484d5d27c86c99283b781d5a
heiheihei <980987346@qq.com>
2021-06-11 20:59:34 +0800

    0.1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Tank War");
+        frame.setSize(800,600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
